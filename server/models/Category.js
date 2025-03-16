8d800bdfcf0e171e24454103b871738612997421
shahadThayyil <shahadthayyil1@gmail.com>
2025-03-16 15:17:14 +0530

    fist commit

---
+++ server/models/Category.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const categorySchema = new mongoose.Schema({
+    name: { type: String, required: 'This  fild is required' },
+    image:{type:String,required:'This fild is required'}
+})
+
+module.exports = mongoose.model('category',categorySchema);
